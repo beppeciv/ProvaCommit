30ac640fed5b909e64421ce2cc583c5aeb443fdd
beppe <giuseppe.civaschi@mail.polimi.it>
2021-03-04 14:23:38 +0100

    Initial commit

---
+++ src/main/java/it/pli/mr/App.java
@@ -0,0 +1,13 @@
+package it.pli.mr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hfirst commit" );
+    }
+}
